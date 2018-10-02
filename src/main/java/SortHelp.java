17feba3bfa807fe1a0e1e1d836b4ac30ffc2a580
LieutenantPeacock <lieutenant.peacock@gmail.com>
2018-10-02 11:45:05 -0400

    SortHelp.

---
+++ src/main/java/SortHelp.java
@@ -0,0 +1,15 @@
+/**
+ * Main class for printing usage instructions.
+ * @author LieutenantPeacock
+ *
+ */
+public class SortHelp {
+	public static void main(final String[] args) {
+		System.out.println("Lt. Peacock's Multisorter");
+		System.out.println();
+		System.out.println("To sort XML          : java -cp lp-multisorter.jar XMLSorter         [inputFile] [outputFile]");
+		System.out.println();
+		System.out.println("If outputFile is not specified, the output is to stdout.");
+		System.out.println("If inputFile is not specified, input is taken from stdin.");
+	}
+}
