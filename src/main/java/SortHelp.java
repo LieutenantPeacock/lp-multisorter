fac534632c9c2f1a661d2033f09d776354016ab8
LieutenantPeacock <lieutenant.peacock@gmail.com>
2020-04-12 14:20:11 -0400

    SortHelp: add instructions on using OpenApiJSONSorter.

--- src/main/java/SortHelp.java
+++ src/main/java/SortHelp.java
@@ -8,6 +8,7 @@ public class SortHelp {
 		System.out.println("Lt. Peacock's Multisorter");
 		System.out.println();
 		System.out.println("To sort XML          : java -cp lp-multisorter.jar XMLSorter         [inputFile] [outputFile]");
+		System.out.println("To sort OpenAPI JSON : java -cp lp-multisorter.jar OpenApiJSONSorter [inputFile] [outputFile]");
 		System.out.println();
 		System.out.println("If outputFile is not specified, the output is to stdout.");
 		System.out.println("If inputFile is not specified, input is taken from stdin.");
