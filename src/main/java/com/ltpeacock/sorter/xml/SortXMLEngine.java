323787d3c32c08d268b9ff5aeeee8bae06ce65c0
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-14 17:21:17 -0400

    SortXMLEngine: clean up unused code.

--- src/main/java/com/ltpeacock/sorter/xml/SortXMLEngine.java
+++ src/main/java/com/ltpeacock/sorter/xml/SortXMLEngine.java
@@ -11,15 +11,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Queue;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -29,12 +26,6 @@ import javax.xml.parsers.SAXParserFactory;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.CDATASection;
 import org.w3c.dom.Comment;
@@ -111,23 +102,6 @@ public class SortXMLEngine {
         }
     }
 
-    private static String removeExtraWsdlpartClose(final String inputStr) {
-        final String s1 = removeExtraClose(inputStr, "wsdl:part");
-        final String s2 = removeExtraClose(s1, "wsdl:input");
-        final String s3 = removeExtraClose(s2, "wsdl:output");
-        final String s4 = removeExtraClose(s3, "xs:element");
-        final String s5 = s4.replaceFirst("\\?><wsdl:definitions", "\\?>\r\n<wsdl:definitions");
-        final String s6 = s5.replaceAll("\" >", "\">");
-        return s6;
-    }
-    
-    private static String removeExtraClose(final String inputStr, final String tag) {
-        final Pattern pattern = Pattern.compile("(<" + tag + " .+ )>\\r?\\n?[ \t]*</" + tag + ">", Pattern.MULTILINE);
-        final Matcher m = pattern.matcher(inputStr);
-        final String prettyXml = m.replaceAll("$1/>");
-        return prettyXml;
-    }
-    
     private ElementVO collectElements(final Element element, final Queue<List<ElementAttribute>> queue) {
     	final ElementVO newElem = new ElementVO(element, queue.poll());
     	final NodeList nodeList = element.getChildNodes();
@@ -204,20 +178,6 @@ public class SortXMLEngine {
         	}
             writer.writeEndElement();
         }
-    }
-
-    public static void printDocument(final Document doc, final OutputStream out)
-            throws IOException, TransformerException {
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer = tf.newTransformer();
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-        LOG.info("===== printing XML =====");
-        transformer.transform(new DOMSource(doc),
-             new StreamResult(new OutputStreamWriter(out, "UTF-8")));
     }
 
     private Document readXml(final InputStream is) {
