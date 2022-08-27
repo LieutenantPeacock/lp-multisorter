2e144d807bd06cee3cccd9d39a17c7c0d950e3fa
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-26 20:57:10 -0400

    Refactor all main sorter classes to extend AbstractSorterCli.

--- src/main/java/OpenApiJSONSorter.java
+++ src/main/java/OpenApiJSONSorter.java
@@ -1,10 +1,6 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
+import com.ltpeacock.sorter.ContentSorter;
 import com.ltpeacock.sorter.json.SortOpenApiJSONEngine;
 
 /**
@@ -12,50 +8,13 @@ import com.ltpeacock.sorter.json.SortOpenApiJSONEngine;
  * @author LieutenantPeacock
  *
  */
-public class OpenApiJSONSorter {
+public class OpenApiJSONSorter extends AbstractSorterCli {
 	public static void main(final String[] args) throws IOException {
-		switch (args.length) {
-		case 2: {
-			final File inputFile = new File(args[0]), outputFile = new File(args[1]);
-			try (FileInputStream fis = new FileInputStream(inputFile);
-					FileOutputStream fos = new FileOutputStream(outputFile)) {
-				run(fis, fos, inputFile.toString(), outputFile.toString());
-			}
-			break;
-		}
-		case 1:
-			if ("--help".equals(args[0])) {
-				help();
-			} else {
-				final File inputFile = new File(args[0]);
-				try (FileInputStream fis = new FileInputStream(inputFile)) {
-					run(fis, System.out, inputFile.toString(), "stdout");
-				}
-			}
-			break;
-		case 0:
-			run(System.in, System.out, "stdin", "stdout");
-			break;
-		default:
-			System.err.println("Too many arguments.");
-			break;
-		}
+		new OpenApiJSONSorter().run(args);
 	}
 
-	private static void run(final InputStream is, final OutputStream os, final String inputName,
-			final String outputName) throws IOException {
-		final long startMs = System.currentTimeMillis();
-		if (os != System.out)
-			System.out.format("Reading from '%s' ...%n", inputName);
-		final SortOpenApiJSONEngine engine = new SortOpenApiJSONEngine();
-		engine.sort(is, os);
-		final long endMs = System.currentTimeMillis();
-		final long tookMs = endMs - startMs;
-		if (os != System.out)
-			System.out.format("Wrote to '%s' ..., took %s ms.%n", outputName, tookMs);
-	}
-
-	private static void help() {
-		System.out.println("Usage: [inputFile] [outputFile]");
+	@Override
+	protected ContentSorter getSortEngine() {
+		return new SortOpenApiJSONEngine();
 	}
 }
--- src/main/java/XMLSorter.java
+++ src/main/java/XMLSorter.java
@@ -1,12 +1,6 @@
-import static com.ltpeacock.sorter.xml.Util.logAndThrow;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
+import com.ltpeacock.sorter.ContentSorter;
 import com.ltpeacock.sorter.xml.SortXMLEngine;
 
 /**
@@ -14,53 +8,13 @@ import com.ltpeacock.sorter.xml.SortXMLEngine;
  * @author LieutenantPeacock
  *
  */
-public class XMLSorter {
-    public static void main(String[] args) {
-        switch (args.length) {
-        case 2: {
-        	final File inputFile = new File(args[0]), outputFile = new File(args[1]);
-            try(FileInputStream fis = new FileInputStream(inputFile); 
-            		FileOutputStream fos = new FileOutputStream(outputFile)){
-            	run(fis, fos, inputFile.toString(), outputFile.toString());
-            } catch (IOException e) {
-				logAndThrow(e);
-			}
-            break;
-        }
-        case 1:
-            if ("--help".equals(args[0])) {
-                help();
-            } else {
-            	final File inputFile = new File(args[0]);
-                try(FileInputStream fis = new FileInputStream(inputFile)){
-                	run(fis, System.out, inputFile.toString(), "stdout");
-                } catch (IOException e) {
-					logAndThrow(e);
-				}
-            }
-            break;
-        case 0:
-            run(System.in, System.out, "stdin", "stdout");
-            break;
-        default:
-            System.err.println("Too many arguments.");
-            break;
-        }
-    }
-    
-    private static void run(final InputStream is, final OutputStream os, final String inputName, final String outputName) {
-        final long startMs = System.currentTimeMillis();
-        if(os != System.out) 
-        	System.out.format("Reading from '%s' ...%n", inputName);
-        SortXMLEngine engine = new SortXMLEngine();
-        engine.sort(is, os);
-        final long endMs = System.currentTimeMillis();
-        final long tookMs = endMs - startMs;
-        if(os != System.out)
-            System.out.format("Wrote to '%s' ..., took %s ms.%n", outputName, tookMs);
+public class XMLSorter extends AbstractSorterCli {
+    public static void main(final String[] args) throws IOException {
+    	new XMLSorter().run(args);
     }
 
-    private static void help() {
-        System.out.println("Usage: [inputFile] [outputFile]");
-    }
+	@Override
+	protected ContentSorter getSortEngine() {
+		return new SortXMLEngine();
+	}
 }
